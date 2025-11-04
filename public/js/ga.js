9062b5b186f52a16554efb746874ffb79062c0dd
FLUKEDEV <wasan95276@gmail.com>
2025-11-05 05:54:20 +0700

    update: enhance explore page (image + design)

--- public/js/ga.js
+++
@@ -1,7 +0,0 @@
-// public/js/ga.js ✅ CSP Safe Version
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){ dataLayer.push(arguments); }
-
-gtag("js", new Date());
-gtag("config", "G-VGBZS316J9");
